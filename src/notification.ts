05e42a9b0074a5f7108725b40e254f89165f39a8
Liyu <wangliyu@huoxing24.com>
2022-07-28 17:58:02 +0800

    fix node-fetch

--- src/notification.ts
+++ src/notification.ts
@@ -1,15 +1,20 @@
 import {INotification} from './types'
 import {getNotification} from './keystore'
-import fetch from "node-fetch";
+import { RequestInfo, RequestInit } from 'node-fetch';
+
+const fetch = (url: RequestInfo, init?: RequestInit) =>
+    import('node-fetch').then(({ default: fetch }) => fetch(url, init));
 
 export const pushNotification = async (topic: string) => {
     console.log('notification.webhook')
     const notifications = await getNotification(topic)
     if (notifications && notifications.length) {
-        notifications.forEach((notification: INotification) => {
+
+        for (const notification of notifications) {
+            // notifications.forEach((notification: INotification) => {
             console.log('notification.webhook', notification.webhook)
             // fetch.post(notification.webhook, { topic })
-            fetch(notification.webhook, {method: 'POST', body: {topic}});
-        })
+            await fetch(notification.webhook, {method: 'POST', body: {topic}});
+        }
     }
 }
